59ec1ffcbba7cdb6b340a12e68a335fd5a7455f7
Nagi-ei <nakt4453@gmail.com>
2024-12-30 12:09:52 +0900

    ✨ Implement TodoList component
    added overall structure

---
+++ src/components/todo-list.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function TodoList({ todoList, setTodoList }) {
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <li key={todo.id} className='todo'>
+          <input type='checkbox' />
+          {todo.name}
+          <input type='text' />
+          <button onClick={handleEdit}>수정</button>
+          <button onClick={handleDelete}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
